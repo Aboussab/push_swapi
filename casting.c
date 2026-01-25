1ff727fbcef1e7d7a3bb67c06fb58289f60dd8ab
amine-XB8999999999999 <aminebossaboune@gmail.com>
2026-01-25 20:02:52 +0100

    fuck

--- casting.c
+++ casting.c
@@ -6,20 +6,28 @@
 /*   By: aboussab <aboussab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/25 11:43:00 by aboussab          #+#    #+#             */
-/*   Updated: 2026/01/25 12:34:56 by aboussab         ###   ########.fr       */
+/*   Updated: 2026/01/25 20:02:05 by aboussab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
 
-t_list	*casting(char *str)
+t_list	valide_arg(char *str)
 {
-	int	i;
+	char	**ptr;
+	int		i;
+	t_list **head;
+	t_list **tmp;
 
 	i = 0;
-	while(i < ft_strlen(str))
+	ptr = ft_split(str,' ');
+	while(i < w_count(str))
 	{
-		
-		
+		head = ft_lstnew(ft_atoi(ptr[i]));
+		i++;
+		tmp = ft_lstnew(ft_atoi(ptr[i]));
+		ft_lstadd_back();
 	}
+	
 }
+
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aboussab <aboussab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/25 11:43:07 by aboussab          #+#    #+#             */
-/*   Updated: 2026/01/25 11:43:08 by aboussab         ###   ########.fr       */
+/*   Updated: 2026/01/25 18:29:42 by aboussab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,7 @@ int main(int argc,char **argv)
 
 	if (argc == 1)
 		return (0);
-	i = 0;
-	while(i < argc)
-	{
-		if
-
-	}
-	
-	write(1,"Error\n",6);
+	i = w_count(argv[1],' ');
+	printf("%d",i);
 	return (0);
 }
