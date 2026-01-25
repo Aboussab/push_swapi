7bcb7663f23e4011515377bc7a9f2c26503b6afb
amine-XB8999999999999 <aminebossaboune@gmail.com>
2026-01-25 13:43:16 +0100

    loool

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# push_swapi
-The Push swap project is a simple yet highly structured algorithmic challenge.calculates and displays the shortest sequence of Push_swap instructions needed to sort the given integers
+*This project has been created as part of the 42 curriculum by aboussab.*
+# push_swap
+The Push swap project is a simple yet highly structured algorithmic challenge.calculates and displays the shortest sequence of Push_swap instructions needed to sort the given integers,It's a C program that calculates and displays the shortest sequence of Push_swap instructions needed to sort the given integers
---
+++ push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboussab <aboussab@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/25 11:43:13 by aboussab          #+#    #+#             */
+/*   Updated: 2026/01/25 11:43:14 by aboussab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "libft/libft.h"
+
+
+
+
+
+#endif
